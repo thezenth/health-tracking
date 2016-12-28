50b0bd306044bb8af99969d952ff6e79cfd8ec2d
Noah Wolfe <darthnoah.wolfe@gmail.com>
2016-12-28 11:36:43 -0500

    Added basic form generation
    - Form generation is based upon passed arrays; e.g., an array is passed
    for all string (or "text") type inputs, and then another for all
    int/float inputs

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ router.use('/users', require('./users'))
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', strArray: ["pizza"] });
 });
 
 module.exports = router;
