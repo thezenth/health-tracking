25948ab652f7ad71494340f151f5933e4df7f687
Noah Wolfe <darthnoah.wolfe@gmail.com>
2016-12-28 11:54:31 -0500

    Complete food form
    - Created misc.json to hold the names of every field
    - Included 'fs' in order to handle file system + load misc.json

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs'); // for loading files/dealing with the filesystem
+
 router.use('/users', require('./users'))
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', strArray: ["pizza"] });
+  fs.readFile('./misc.json', function(err, data) { //note: . is used instead of .. because the "reference" directory is the directory that app.js is running in
+    if (err) {
+      throw err;
+    } else {
+      console.log(data);
+      var jsonData = JSON.parse(data);
+      res.render('index', { title: 'Express', strArray: jsonData.food_string_values, numArray: jsonData.food_number_values });
+    }
+  });
 });
 
 module.exports = router;
