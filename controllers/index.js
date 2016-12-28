72e2f2d6a31ff91657a545d55233b65702436f7b
Noah Wolfe <darthnoah.wolfe@gmail.com>
2016-12-28 13:28:44 -0500

    Added ability to save a "food" to MongoDB from HTML form
    - Cleaned up a few debug messages
    - Added misc.json to include an array of "name" values for input "name",
    which reflects the fields in food.js

--- controllers/index.js
+++ controllers/index.js
@@ -8,19 +8,27 @@ var Food = require('../models/food'); // is this extraneous?
 router.use('/users', require('./users'))
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+
   fs.readFile('./misc.json', function(err, data) { //note: . is used instead of .. because the "reference" directory is the directory that app.js is running in
     if (err) {
       throw err;
     } else {
-      console.log(data);
+      // console.log(data);
       var jsonData = JSON.parse(data);
 
       Food.find({}, function(err, foods) {
-        res.render('index', { title: 'Express', strArray: jsonData.food_string_values, numArray: jsonData.food_number_values, mealDateArray: jsonData.meal_datetime_values, allFoods: foods });
+        res.render('index', { title: 'Express', strArray: jsonData.food_string_values, foodStringNames: jsonData.food_string_value_names, numArray: jsonData.food_number_values, foodNumberNames: jsonData.food_number_value_names, mealDateArray: jsonData.meal_datetime_values, allFoods: foods });
       });
     }
   });
 });
+
+router.post('/', function(req, res) {
+  //console.dir(req.body);
+  //console.dir(Object.keys(Food.schema.obj));
+  var newFood = new Food( req.body );
+  newFood.save();
+})
 
 module.exports = router;
