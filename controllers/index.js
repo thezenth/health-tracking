4e03861f7a57b42c89348e969586d2c0f51aaa49
Noah Wolfe <darthnoah.wolfe@gmail.com>
2016-12-28 12:16:13 -0500

    Added meal form
    - Added dynamic generation of a checkbox list of foods, based upon foods
    listed in MongoDB
    - Added ability to enter date, using input type "datetime-local"
    - Todo: Include timezone based upon browser/computer response

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var fs = require('fs'); // for loading files/dealing with the filesystem
 
+var Food = require('../models/food'); // is this extraneous?
+
 router.use('/users', require('./users'))
 
 /* GET home page. */
@@ -13,7 +15,10 @@ router.get('/', function(req, res, next) {
     } else {
       console.log(data);
       var jsonData = JSON.parse(data);
-      res.render('index', { title: 'Express', strArray: jsonData.food_string_values, numArray: jsonData.food_number_values });
+
+      Food.find({}, function(err, foods) {
+        res.render('index', { title: 'Express', strArray: jsonData.food_string_values, numArray: jsonData.food_number_values, mealDateArray: jsonData.meal_datetime_values, allFoods: foods });
+      });
     }
   });
 });
