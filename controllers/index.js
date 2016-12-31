d10d52b4e67520a3652a51932d1cc2a3e1ee3edb
Noah Wolfe <darthnoah.wolfe@gmail.com>
2016-12-31 10:13:10 -0500

    Added basic USDA API request

--- controllers/index.js
+++ controllers/index.js
@@ -18,18 +18,29 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
 
-  /*var apiKey = "FYpMQAWPYLHGPJvmgvtGqNeSStYiFlSgy9Wn3YXC";
+  var apiKey = "FYpMQAWPYLHGPJvmgvtGqNeSStYiFlSgy9Wn3YXC";
   var query = req.body.foodQuery;
   var searchUrl = `http://api.nal.usda.gov/ndb/search/?format=json&q=${query}&sort=r&max=10&offset=0&api_key=${apiKey}`;
   request(searchUrl, function(err, response, body) {
     if (err) {
       throw err;
     } else if (response.statusCode == 200) {
-      res.
+      var items = body.list.item;
+      var dbNumbers = [];
+      for(var i = 0; i<items.length; i++) {
+        dbNumbers.push(items[i].ndbno);
+
+        // wait till it gets through everything to build query string and redirect
+        if(i == items.length - 1) {
+          console.log(dbNumbers);
+          //var queryString = "?items=" + dbNumbers.toString();
+          //res.redirect('/searchResults' + queryString);
+        }
+      }
     } else {
       console.error(response.statusCode);
     }
-  });*/
+  });
 });
 
 module.exports = router;
