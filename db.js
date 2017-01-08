73863af24a6369cdd04bdaafc30d426595f72bbc
Noah Wolfe <darthnoah.wolfe@gmail.com>
2017-01-08 14:01:06 -0500

    Fixed minor typo in db.js
    - It is process.env.hasOwnProperty, not process.env.hasOwnPropert

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var cloudant = {
   url: "https://5e2a3253-651f-411b-8c00-76b0dd7ed0af-bluemix:440cbbceccfa7f47580095a88bc001926a0deac672f1a76f9b07f7f11beae71e@5e2a3253-651f-411b-8c00-76b0dd7ed0af-bluemix.cloudant.com"
 };
 
-if (process.env.hasOwnPropert("VCAP_SERVICES")) {
+if (process.env.hasOwnProperty("VCAP_SERVICES")) {
   var env = JSON.parse(process.env.VCAP_SERVICES);
 
   cloudant = env['cloudantNoSQLDB'][0].credentials;
